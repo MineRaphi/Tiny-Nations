515d65b243f3164ef25337fe81d4c7243390e475
MineRaphi <MineRaphiHD@gmail.com>
2026-06-16 13:13:55 +0200

    added year progression with deaths and births

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <random>
 #include "nation.h"
 
 using namespace std;
 
-void printState(Nation* nations, int count);
+mt19937 rng(random_device{}());
+
+void printState(Nation* nations, int count, int year);
+void progressYear(Nation* nations, int count, int* year);
+int calculateDeaths(int population, float happiness);
+int calculateBirths(int population, float happiness);
 
 int main(void) {
     int nation_count = 3;
+    int current_year = 0;
+
     string names[5] = {"red", "blue", "green", "yellow", "purple"};
 
     cout << "Wecome to Tiny Nation Simulator!\n";
     cout << "how many nations do you want for the simulation? (2-5)\n";
     cout << "!! For development reasons, the nation amount will be set to 3 !!\n\n\n";
 
-    Nation redNation("red");
-    Nation blueNation("blue");
-    Nation greenNation("green");
+    Nation redNation("Red");
+    Nation blueNation("Blue");
+    Nation greenNation("Green");
 
     Nation nations[] = { redNation, blueNation, greenNation };
 
@@ -27,12 +35,56 @@ int main(void) {
         nations[i].m_happiness = 0.7;
     }
 
-    printState(nations, nation_count);
-}
+    for(int i=0; i < nation_count; i++) {
+        nations[i].m_stateMoney = 50;
+    }
 
-void printState(Nation* nations, int count) {
-    for(int i=0; i < count; i++) {
-        cout << nations[i].m_name << "\tPopulation: " << nations[i].m_population << " \tHappiness: " << int(nations[i].m_happiness*100) << "%\n";
+    while (1) {
+        printState(nations, nation_count, current_year);
+        getchar();
+
+        progressYear(nations, nation_count, &current_year);
     }
 }
 
+void printState(Nation* nations, int count, int year) {
+    cout << "Current year: " << year << "\n=================\n";
+    for(int i=0; i < count; i++) {
+        cout << nations[i].m_name << "\tPopulation: " << nations[i].m_population
+        <<"  Happiness: " << int(nations[i].m_happiness*100) << "%"
+        << "  Money: " << nations[i].m_stateMoney << "k"
+        << "\n";
+    }
+}
+
+void progressYear(Nation* nations, int count, int* year) {
+    (*year)++;
+
+    for(int i=0; i < count; i++) {
+        int deaths = calculateDeaths(nations[i].m_population, nations[i].m_happiness);
+        int births = calculateBirths(nations[i].m_population, nations[i].m_happiness);
+
+        nations[i].m_population -= deaths;
+        nations[i].m_population += births;
+    }
+}
+
+int calculateDeaths(int population, float happiness) {
+    float average_death = 0.07;
+    float happiness_mult = 0.3;
+
+    uniform_real_distribution<float> dist(0.7f, 1.5f);
+    float variance = dist(rng);
+
+    return population * average_death * variance * (1 - happiness * happiness_mult);
+}
+
+int calculateBirths(int population, float happiness) {
+    float average_birth = 0.07;
+    float happiness_mult = 0.2;
+
+    uniform_real_distribution<float> dist(0.7f, 1.4f);
+    float variance = dist(rng);
+
+    return population * average_birth * variance * (1 + happiness * happiness_mult);
+}
